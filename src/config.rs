87fe4d2ed11922b7e6816932322e7e8687cb1778
ljmam <ljmam1208@gmail.com>
2025-03-06 16:46:56 +0800

    修改中继

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.990aaa.org"];
+pub const RS_PUB_KEY: &str = "X7cyME6Ro9s18xTI8ePwIvnOmZL5Zhy6lXeUigFjJZE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
